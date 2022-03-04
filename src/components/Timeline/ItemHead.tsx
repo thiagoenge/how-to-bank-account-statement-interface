e11f780342029f7ac9181f9ebdaa29dac5b92eb2
Thiago Enge <thiago.enge@ahazou.com>
2022-03-04 13:43:27 -0300

    fix interface error when filter

--- src/components/Timeline/ItemHead.tsx
+++ src/components/Timeline/ItemHead.tsx
@@ -1,12 +1,13 @@
 import { TimelineItemHead } from 'src/interfaces'
 import { formatCurrency } from 'src/utils/handle-currency'
+import { parseItemHeadDate } from 'src/utils/handle-dates'
 import style from './ItemHead.module.css'
 
 const TimelineItemHead = ({date,amountTotal, isFirst}:TimelineItemHead) => {
   return (
     <div className={style.timelineItemHeadWrapper}>
       <div className={style.timelineDate}>
-        {date}
+        {parseItemHeadDate(date)}
       </div>  
       <div className={style.timelineTransactionType}>
         {isFirst ? <span className={style.timelineHead}>Tipo de Transação</span> : null }
